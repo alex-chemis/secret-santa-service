8ecbf77a6e0fab8dfb7a9e7b52f053c7bfcab4f6
https://gitlab.com/alxChemis <4lxchemis@gmail.com>
2023-01-07 00:01:22 +0300

    Coding guideline

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -4,9 +4,3 @@ pub mod groups;
 pub mod members;
 pub mod santas;
 pub mod users;
-
-#[derive(Serialize, Deserialize, Debug)]
-#[serde(crate = "rocket::serde")]
-pub struct ApiError {
-    pub details: String,
-}
