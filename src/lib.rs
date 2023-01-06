d1b05b39e1e298210a5049d65f85a55015aacd28
https://gitlab.com/alxChemis <4lxchemis@gmail.com>
2023-01-06 08:03:56 +0300

    Connected database

--- src/lib.rs
+++ src/lib.rs
@@ -4,5 +4,7 @@ extern crate rocket;
 #[macro_use]
 extern crate diesel;
 
+pub mod database;
 pub mod errors;
+pub mod models;
 pub mod schema;
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 #[macro_use]
 extern crate rocket;
 
+use secret_santa_service_example::database::PgConnection;
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
+        .attach(PgConnection::fairing())
 }
